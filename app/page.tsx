660c9ae77655b25c5df92aba61ee759ae1212ce6
Piotr Dziedzina <p.dziedzina@o2.pl>
2023-11-24 13:02:36 +0100

    Remove unnecessary code from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 "use client";
-import { useAppSelector } from "@/redux/hooks";
-import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-	const { userId } = useAppSelector((state) => state.user);
-	const { data, isLoading } = useQuery({
-		queryKey: ["user", userId],
-	});
-
 	return <div style={{ height: "10000px", width: "100%" }}>Home Page</div>;
 }
