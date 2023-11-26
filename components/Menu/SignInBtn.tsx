0aeabd168cea693676634ef387f909b864c275e0
Piotr Dziedzina <p.dziedzina@o2.pl>
2023-11-26 20:58:56 +0100

    Temporary implementation of fetching user data in SignInBtn

--- components/Menu/SignInBtn.tsx
+++ components/Menu/SignInBtn.tsx
@@ -1,14 +1,29 @@
 "use client";
 import { setUserId } from "@/redux/features/user/userSlice";
-import { useAppDispatch } from "@/redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { nanoid } from "@reduxjs/toolkit";
+import { useQuery } from "@tanstack/react-query";
 import React, { useEffect, useState } from "react";
-import { LogInBtn } from "./btns";
+import { LoadingBtn, LogInBtn, SignOutBtn } from "./btns";
 
 const SignInBtn: React.FC = () => {
 	const dispatch = useAppDispatch();
 	// determines if sign up form should be displayed
 	const [openPanel, setOpenPanel] = useState(false);
+
+	const { userId } = useAppSelector((state) => state.user);
+	const { data, isLoading } = useQuery({
+		enabled: typeof userId === "string",
+		queryKey: ["user", userId],
+		queryFn: async () => {
+			try {
+				return "";
+			} catch (error) {
+				console.error(error);
+				return null;
+			}
+		},
+	});
 
 	useEffect(() => {
 		dispatch(setUserId(nanoid(64)));
@@ -16,7 +31,13 @@ const SignInBtn: React.FC = () => {
 
 	return (
 		<div>
-			<LogInBtn handleClick={() => setOpenPanel(true)} />
+			{isLoading ? (
+				<LoadingBtn />
+			) : data ? (
+				<SignOutBtn handleClick={() => {}} />
+			) : (
+				<LogInBtn handleClick={() => setOpenPanel(true)} />
+			)}
 			{openPanel && <div>SignInForm</div>}
 		</div>
 	);
