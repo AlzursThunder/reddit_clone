c522c5f73fd4b8aa6181261360c89e15e38fa6e1
Piotr Dziedzina <p.dziedzina@o2.pl>
2023-11-28 18:36:47 +0100

    Create Template for LogInForm & RegisterForm

---
+++ components/SignInForm/Template.tsx
@@ -0,0 +1,33 @@
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import React from "react";
+import styles from "./SignInForm.module.css";
+
+interface props {
+	header: string;
+	announcement: string;
+	children: React.ReactNode;
+}
+
+const SignInTemplate: React.FC<props> = ({
+	children,
+	header,
+	announcement,
+}) => {
+	return (
+		<>
+			<div className={styles.template}>
+				<Typography
+					variant="h5"
+					fontWeight={"bold"}
+				>
+					{header}
+				</Typography>
+				<Typography variant="body2">{announcement}</Typography>
+				<Stack spacing={3}>{children}</Stack>
+			</div>
+		</>
+	);
+};
+
+export default SignInTemplate;
