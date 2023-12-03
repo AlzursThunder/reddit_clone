0fb526d817b7b2f257088189e715e6b87bf21553
Piotr Dziedzina <p.dziedzina@o2.pl>
2023-12-03 21:24:49 +0100

    Create new custom component for form button

--- components/SignInForm/CustomComponents.tsx
+++ components/SignInForm/CustomComponents.tsx
@@ -1,4 +1,5 @@
 import { styled } from "@mui/material";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
@@ -22,6 +23,11 @@ export const CustomTypography = styled("span")(({ theme }) => ({
 export const CustomLink = styled(Link)(({ theme }) => ({
 	color: theme.palette.primary.dark,
 	...styles,
+}));
+
+export const CustomFormButton = styled(Button)(({ theme }) => ({
+	borderRadius: "var(--rounded-full)",
+	width: "100%",
 }));
 
 export const ReqLabel: React.FC<{ text: string }> = ({ text }) => (
