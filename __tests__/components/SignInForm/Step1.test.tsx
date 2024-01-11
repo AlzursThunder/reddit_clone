7f21c07b39f523cb4b49a99e934a7df2ca44738b
Piotr Dziedzina <p.dziedzina@o2.pl>
2024-01-11 19:30:05 +0100

    test: Write tests for FirstStep component

---
+++ __tests__/components/SignInForm/Step1.test.tsx
@@ -0,0 +1,106 @@
+import Step1 from "@/components/SignInForm/RegisterForm/Step1";
+import store from "@/redux/store";
+import {
+	cleanup,
+	fireEvent,
+	render,
+	screen,
+	waitFor,
+} from "@testing-library/react";
+import { Provider } from "react-redux";
+
+const mockHandleChange = jest.fn();
+
+const { registerForm } = store.getState().signInForm;
+
+afterEach(cleanup);
+
+describe("First Step", () => {
+	describe("Render", () => {
+		it("should render the first step", () => {
+			const { getByTestId } = render(
+				<Step1
+					email=""
+					handleChange={mockHandleChange}
+					errorMessages={{ email: null }}
+				>
+					""
+				</Step1>
+			);
+			const input = getByTestId("step1-input-email");
+			expect(input).toBeInTheDocument();
+		});
+
+		it("should render the error message if the email is invalid", () => {
+			const { getByTestId } = render(
+				<Step1
+					email="test@@d.d"
+					handleChange={mockHandleChange}
+					errorMessages={{
+						email: {
+							message: "Invalid email address",
+							kind: "invalid",
+							name: "email",
+							path: "email",
+							properties: {
+								type: "string",
+								message: "Invalid email address",
+								path: "email",
+							},
+						},
+					}}
+				>
+					""
+				</Step1>
+			);
+			const errAlertEl = getByTestId("error-email");
+			expect(errAlertEl).toBeInTheDocument();
+			expect(errAlertEl).toHaveTextContent("Invalid email address");
+		});
+	});
+
+	describe("User interaction", () => {
+		it("should call the handleChange function when the input changes", async () => {
+			const { getByTestId } = render(
+				<Provider store={store}>
+					<Step1
+						email={registerForm.email}
+						handleChange={mockHandleChange}
+						errorMessages={{ email: null }}
+					>
+						""
+					</Step1>
+				</Provider>
+			);
+			const input = getByTestId("step1-input-email");
+
+			fireEvent.change(input, { target: { value: "test@test.com" } });
+
+			waitFor(() => {
+				expect(mockHandleChange).toHaveBeenCalledTimes(1);
+			});
+		});
+
+		it("should update the email state when the input changes", async () => {
+			const { getByTestId } = render(
+				<Provider store={store}>
+					<Step1
+						email={registerForm.email}
+						handleChange={mockHandleChange}
+						errorMessages={{ email: null }}
+					>
+						""
+					</Step1>
+				</Provider>
+			);
+			const input = getByTestId("step1-input-email");
+
+			fireEvent.change(input, { target: { value: "test@test.com" } });
+
+			waitFor(() => {
+				expect(input).toHaveValue("test@test.com");
+				expect(registerForm.email).toBe("test@test.com");
+			});
+		});
+	});
+});
